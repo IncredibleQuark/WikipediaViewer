cc6653cd606f01e74906cd71b3434c1aaa8f805b
IncredibleQuark <lkupinski@gmail.com>
2017-10-14 20:24:17 +0200

    aded autocomplete jquery UI

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-
-var box = document.getElementById('searchBox');
-
-box.addEventListener('input', function () {
-    var word = box.value;
+$(document).ready(function () {
+    
+    var box = $('#searchBox').on('keyup', function () {
+    var word = box.val();
 
     console.log('takkk');
 
@@ -10,12 +9,18 @@ box.addEventListener('input', function () {
         url: 'https://en.wikipedia.org//w/api.php?action=opensearch&format=json&prop=revisions&rvprop=content&search='+word,
         method: 'get',
         dataType: 'JSON',
+        headers: ["Content-Type", "application/json; charset=UTF-8"],
         success: function (data) {
-            console.log(data);
+            var auto = box.autocomplete({
+                source: data[1]
+            });
+
         },
         error: function () {
             console.log('Ups, something went wrong!');
         }
     });
+
+});
 
 });
